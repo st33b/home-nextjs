dacb63bde7e1ca245cd76df3374e90e024fb4232
Scott Stebleton <scott@stebleton.net>
2022-09-14 00:19:24 -0400

    update npm packages, small tweaks to make new versions happy

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,12 @@
 import '../styles/globals.css'
+import {SSRProvider} from "react-bootstrap";
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <SSRProvider>
+      <Component {...pageProps} />
+    </SSRProvider>
+  )
 }
 
 export default MyApp
